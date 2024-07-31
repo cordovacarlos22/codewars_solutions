f4426e0ff219670df232b9127f7eea271450e10b
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-31 10:03:29 -0700

    feat: :sparkles: added solution to Counting sheep...

---
+++ Counting sheep.../script.js
@@ -0,0 +1,12 @@
+function countSheeps(arrayOfSheep) {
+  let sheeps = 0;
+
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    arrayOfSheep[i] == true ? sheeps++ : null
+  }
+
+
+
+  return sheeps
+
+} 
