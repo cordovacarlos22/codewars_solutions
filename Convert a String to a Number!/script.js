55555fcb6bd6a7f224c7257b9641ce20cda5fcc7
carloscordova.dev <cordovamanuel22@icloud.com>
2024-09-07 16:08:12 -0700

    add solution to problem set convert string to a number

---
+++ Convert a String to a Number!/script.js
@@ -0,0 +1,6 @@
+const stringToNumber = function (str) {
+  // put your code here
+  const convertedStr = str.split().join('')
+  const formatedSting = Number(convertedStr)
+  return formatedSting;
+}
